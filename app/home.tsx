8449fada8ee7f06d3e2fa383f58b923f555f8749
agomezi <alex.gomezd02@gmail.com>
2026-05-12 03:02:32 -0400

    Added scrollview and heading for next cards

--- app/home.tsx
+++ app/home.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatBox, StatBoxRow } from "../components/StatBox";
 import SwipeableCards from "../components/SwipeableCards";
@@ -9,34 +9,44 @@ export default function Home() {
     <SafeAreaView
       style={[commonStyles.container, { backgroundColor: "#0A0A0A" }]}
     >
-      <Text style={commonStyles.headerLogo}>PFP Fight Night BELL</Text>
-      <View style={commonStyles.divider} />
+      <ScrollView>
+        <Text style={commonStyles.headerLogo}>PFP Fight Night BELL</Text>
+        <View style={commonStyles.divider} />
 
-      <SwipeableCards
-        cards={[
-          {
-            tag: "NEXT EVENT",
-            tagColor: "#e5003c",
-            title: "UFC 300",
-            subtitle: "PEREIRA VS HILL",
-            aspectRatio: 1.2,
-            footer: (
-              <StatBoxRow>
-                <StatBox value="02" label="DAYS" />
-                <StatBox value="14" label="HOURS" />
-                <StatBox value="45" label="MINS" />
-              </StatBoxRow>
-            ),
-          },
-          {
-            tag: "YOUR LEAGUE",
-            tagColor: "#E8A020",
-            title: "DTC",
-            subtitle: "You are in 3rd place",
-            aspectRatio: 1.2,
-          },
-        ]}
-      />
+        <SwipeableCards
+          cards={[
+            {
+              tag: "NEXT EVENT",
+              tagColor: "#e5003c",
+              title: "UFC 300",
+              subtitle: "PEREIRA VS HILL",
+              aspectRatio: 1.2,
+              footer: (
+                <StatBoxRow>
+                  <StatBox value="02" label="DAYS" />
+                  <StatBox value="14" label="HOURS" />
+                  <StatBox value="45" label="MINS" />
+                </StatBoxRow>
+              ),
+            },
+            {
+              tag: "YOUR LEAGUE",
+              tagColor: "#E8A020",
+              title: "DTC",
+              subtitle: "You are in 3rd place",
+              aspectRatio: 1.2,
+            },
+          ]}
+        />
+        <View style={commonStyles.cardWrapper}>
+          <Text style={[commonStyles.cardTitle, { textAlign: "left" }]}>
+            Your Performance
+          </Text>
+          <Text style={[commonStyles.cardSubtitle, { marginTop: 9 }]}>
+            View History
+          </Text>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
