b6c6e385f9cba663560f15d17ce8b2611666af8e
agomezi <alex.gomezd02@gmail.com>
2026-05-16 00:42:44 -0400

    Added two new cards

--- app/home.tsx
+++ app/home.tsx
@@ -1,5 +1,6 @@
 import { ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import InfoCards from "../components/InfoCards";
 import { StatBox, StatBoxRow } from "../components/StatBox";
 import SwipeableCards from "../components/SwipeableCards";
 import { commonStyles } from "../styles/common";
@@ -9,7 +10,7 @@ export default function Home() {
     <SafeAreaView
       style={[commonStyles.container, { backgroundColor: "#0A0A0A" }]}
     >
-      <ScrollView>
+      <ScrollView showsVerticalScrollIndicator={false}>
         <Text style={commonStyles.headerLogo}>PFP Fight Night BELL</Text>
         <View style={commonStyles.divider} />
 
@@ -38,14 +39,93 @@ export default function Home() {
             },
           ]}
         />
-        <View style={commonStyles.cardWrapper}>
-          <Text style={[commonStyles.cardTitle, { textAlign: "left" }]}>
+        <View
+          style={[
+            commonStyles.cardWrapper,
+            { marginBottom: 9, justifyContent: "space-between" },
+          ]}
+        >
+          <Text
+            style={[
+              commonStyles.cardTitle,
+              { textAlign: "left", paddingLeft: 3 },
+            ]}
+          >
             Your Performance
           </Text>
-          <Text style={[commonStyles.cardSubtitle, { marginTop: 9 }]}>
+          <Text
+            style={[
+              commonStyles.cardSubtitle,
+              { marginTop: 9, paddingRight: 3 },
+            ]}
+          >
             View History
           </Text>
         </View>
+        <InfoCards
+          cards={[
+            {
+              tag: "CURRENT LEAGUE RANK",
+              tagColor: "#707079",
+              title: "#4",
+              titleSize: 50,
+              footer: (
+                <Text
+                  style={[commonStyles.cardSubtitle, { textAlign: "left" }]}
+                >
+                  Top 5% in &quot;Global Strikers&quot; League
+                </Text>
+              ),
+            },
+            {
+              tag: "LAST EVENT POINTS",
+              tagColor: "#707079",
+              title: "842",
+              titleUnit: "PTS",
+              titleSize: 50,
+              footer: (
+                <>
+                  <View
+                    style={[
+                      commonStyles.divider,
+                      { marginTop: 6, marginBottom: 10 },
+                    ]}
+                  />
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Text
+                      style={[
+                        commonStyles.cardSubtitle,
+                        { textAlign: "left", marginBottom: 0 },
+                      ]}
+                    >
+                      Avg. Score
+                    </Text>
+                    <Text
+                      style={[
+                        commonStyles.cardSubtitle,
+                        {
+                          textAlign: "right",
+                          marginBottom: 0,
+                        },
+                      ]}
+                    >
+                      610
+                    </Text>
+                  </View>
+                </>
+              ),
+            },
+            {
+              tag: "",
+            },
+          ]}
+        />
       </ScrollView>
     </SafeAreaView>
   );
